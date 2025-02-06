5873e4c2e5676b1fa8d7e16231c11e7f4c5f4c75
bmr7211 <133488663+bmr7211@users.noreply.github.com>
2025-02-06 14:38:20 +0900

    Update db.js

--- my-app_fe/backend/database/db.js
+++ my-app_fe/backend/database/db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'bmr72117211',
+  password: '***********',
   database: 'ais'
 });
 
